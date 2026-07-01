ade2aec19b9d6a19c2668a2129b5dd892609fc57
Martijnkop <martijn.koppejan@scanman.com>
2026-07-01 13:08:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Demo - V6',
+    message: 'Demo - het werkt!',
     status: 'running',
     timestamp: new Date().toISOString()
   });
